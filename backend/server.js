38a507c3d51eacfcaa5f947e2b9f40d0988ffbf6
German <metzgergerman@gmail.com>
2025-07-06 04:10:32 -0300

    nuevo link

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // React corre por defecto en 3000
+    origin: "https://knuclebones-production.up.railway.app/", // React corre por defecto en 3000
     methods: ["GET", "POST"]
   }
 });
